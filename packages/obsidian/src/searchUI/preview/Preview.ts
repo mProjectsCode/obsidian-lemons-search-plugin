d8e59b466a0aae3acf732670e2dd84dd2571b55b
Moritz Jung <m.projects.code@gmail.com>
2025-02-26 21:34:49 +0100

    disable pdf previews because they steal focus

--- packages/obsidian/src/searchUI/preview/Preview.ts
+++ packages/obsidian/src/searchUI/preview/Preview.ts
@@ -64,8 +64,9 @@ export async function getPreview(path: string | undefined, plugin: LemonsSearchP
 			return { type: PreviewType.EMPTY_TEXT };
 		}
 		return { type: PreviewType.TEXT, content };
-	} else if (path.endsWith('.pdf')) {
-		return { type: PreviewType.MARKDOWN, content: `![[${path}]]` };
+	// NOTE: disabled due to bug with embedded PDFs stealing focus from the search input
+	// } else if (path.endsWith('.pdf')) {
+	// 	return { type: PreviewType.MARKDOWN, content: `![[${path}]]` };
 	} else if (IMAGE_EXTENSIONS.some(ext => path.endsWith(ext))) {
 		const content = plugin.getResourcePath(path);
 
